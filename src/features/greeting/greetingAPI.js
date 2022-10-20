13e7f6da6cc23b4d520c08e5025e8ace18a16e16
mikemtzp <mikemp8226@gmail.com>
2022-10-20 09:26:04 -0500

    Fix API url to fetch random greeting

--- src/features/greeting/greetingAPI.js
+++ src/features/greeting/greetingAPI.js
@@ -1,5 +1,5 @@
 const getData = async () => {
-  const response = await fetch('/greetings');
+  const response = await fetch('http://localhost:3000/greetings');
   return await response.json();
 }
 
